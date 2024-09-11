88726b86f645dac0643a2de92bf7d962b9136b6e
gsl-anthonymerle <anthony.merle@groupeseloger.com>
2024-09-11 16:30:02 +0200

    Make App's ContentView display future form showcases

--- FormExample/ContentView.swift
+++ FormExample/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum FormShowCase: CaseIterable {
+        case simpleForm
+    }
+
+    let forms = FormShowCase.allCases
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(forms, id: \.self) { form in
+                    NavigationLink(value: form) {
+                        Text(title(for: form))
+                    }
+                }
+            }
+            .navigationTitle("AvivFormValidation")
+            .navigationDestination(for: FormShowCase.self) { showCase in
+                    destinationView(for: showCase)
+                    .navigationTitle(title(for: showCase))
+                }
         }
-        .padding()
+    }
+
+    func title(for showCase: FormShowCase) -> String {
+        switch showCase {
+        case .simpleForm:
+            "User Profile"
+        }
+    }
+
+    func destinationView(for showCase: FormShowCase) -> some View {
+        switch showCase {
+        case .simpleForm:
+            Text(title(for: showCase))
+        }
     }
 }
 
