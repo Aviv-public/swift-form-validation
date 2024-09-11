68492b193f73ce396955fe53b03e146c2d15da57
gsl-anthonymerle <anthony.merle@groupeseloger.com>
2024-09-11 09:39:26 +0200

    Create empty iOS App and empty Swift Package

---
+++ FormExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FormExample
+//
+//  Created by Anthony MERLE on 11/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FormExample/FormExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormExampleApp.swift
+//  FormExample
+//
+//  Created by Anthony MERLE on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct FormExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
